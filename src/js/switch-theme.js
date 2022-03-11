1e4db64267557e77d8e8262998b1fe018c209cfa
Frederick Lopez <mtester010@gmail.com>
2022-03-11 16:04:35 -0600

    Add light mode for left panel content

--- src/js/switch-theme.js
+++ src/js/switch-theme.js
@@ -3,6 +3,12 @@ const lightTheme = document.getElementById('sun');
 const hamburgerBg = document.getElementById('hamburger');
 const pageName = document.querySelector('.page-name');
 const viewMode = document.querySelector('.view-mode');
+const leftIcon = document.querySelector('.profile-icon');
+const persnlInfo = document.querySelector('.personal-info');
+const skillInfo = document.querySelector('.skill-info');
+const socialbutn = document.querySelector('.social-buttons');
+const skillColor = document.querySelectorAll('.skill');
+
 
 
 lightTheme.addEventListener('click', function(e) {
@@ -14,6 +20,13 @@ lightTheme.addEventListener('click', function(e) {
   hamburgerBg.style.backgroundColor = 'rgb(72, 72, 99)';
   pageName.style.backgroundColor = '#f2f3f8'
   viewMode.style.backgroundColor = 'rgb(72, 72, 99)';
+  leftIcon.style.backgroundColor = 'rgb(72, 72,99)';
+  skillInfo.style.backgroundColor = '#f2f3f8';
+  socialbutn.style.backgroundColor = 'rgb(72, 72,99)';
+  persnlInfo.style.color = '#4a4a4a';
+  skillColor.forEach(element => {
+    element.firstElementChild.style.color = '#4a4a4a';
+  });
 })
 
 darkTheme.addEventListener('click', function(e) {
@@ -25,4 +38,11 @@ darkTheme.addEventListener('click', function(e) {
   hamburgerBg.style.backgroundColor = '#24242f';
   pageName.style.backgroundColor = '#20202a';
   viewMode.style.backgroundColor = 'rgb(36,36,47)';
+  leftIcon.style.backgroundColor = 'rgb(36,36,47)';
+  skillInfo.style.backgroundColor = 'rgb(32,32,42)';
+  socialbutn.style.backgroundColor = 'rgb(36,36,47)';
+  persnlInfo.style.color = 'white';
+  skillColor.forEach(element => {
+    element.firstElementChild.style.color = 'white';
+  });
 })
